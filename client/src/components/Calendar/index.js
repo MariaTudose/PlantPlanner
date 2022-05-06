87a4842110376976c4a247f71aac31659d916965
Maria Tudose <maria.tudose@visma.com>
2022-05-06 21:32:52 +0300

    Add date-fns to project and use it in Calendar component

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,109 +1,86 @@
 import React, { useState, useEffect } from 'react';
+import { format, addDays, subDays, addMonths, subMonths, setDate, isEqual, isSameMonth } from 'date-fns';
+import { weekDays, getFirstWeekday, getLastWeekday, getDaysInMonth, plantsToday, getIntervals } from './utils';
 import PlantGrid from '../PlantGrid';
-import { months, weekDays, getFirstWeekDay, getDaysInMonth } from './utils';
 
 import './style.scss';
 
-const plantsToday = (plants, date) =>
-    plants.filter(plant => new Date(plant.wateringDay).toDateString() === date.toDateString());
-
-const CalendarDays = ({ daysInMonth, date, plants, plantsToday, selectDay }) => {
-    return [...Array(daysInMonth)].map((x, day) => {
-        const newDate = new Date(date.getFullYear(), date.getMonth(), day + 1);
-        const intervals = plantsToday(plants, newDate)
-            .map(plants => plants.interval)
-            .reduce((res, interval) => {
-                res[interval] = (res[interval] || 0) + 1;
-                return res;
-            }, {});
-
-        return (
-            <div key={day} className={date.getDate() === day + 1 ? 'today' : ''}>
-                <button onClick={() => selectDay(day + 1, plantsToday)}>{day + 1}</button>
-                {Object.keys(intervals).map(interval => (
-                    <div key={interval}>
-                        {intervals[interval]}x
-                        <button key={interval} onClick={() => selectDay(day + 1, plantsToday)} className="interval">
-                            {interval}
-                        </button>
-                    </div>
-                ))}
+const CalendarDay = ({ date, selectedDate, setSelectedDate, plants }) => (
+    <div className={isEqual(selectedDate, date) ? 'selected' : ''}>
+        <button className={!isSameMonth(selectedDate, date) ? 'other-month' : ''} onClick={() => setSelectedDate(date)}>
+            {format(date, 'd')}
+        </button>
+        {Object.entries(getIntervals(plants, date)).map(([interval, amount]) => (
+            <div key={interval}>
+                {amount}x
+                <button onClick={() => setSelectedDate(date)} className="interval">
+                    {interval}
+                </button>
             </div>
-        );
-    });
+        ))}
+    </div>
+);
+
+const CalendarDays = ({ selectedDate, setSelectedDate, plants }) => {
+    const prevMonthDays = getFirstWeekday(selectedDate);
+    const nextMonthDays = 6 - getLastWeekday(selectedDate);
+    const visibleDays = getDaysInMonth(selectedDate) + prevMonthDays + nextMonthDays;
+
+    return [...Array(visibleDays).keys()].map(i => (
+        <CalendarDay
+            key={i}
+            date={setDate(selectedDate, -prevMonthDays + i + 1)}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            plants={plants}
+        />
+    ));
 };
 
-const Calendar = ({ plants, date, setDate, selectMonth, selectDay, firstWeekDay, daysInMonth }) => (
+const Calendar = ({ plants, selectedDate, setSelectedDate }) => (
     <div className="calendar">
         <div className="calendar-header">
-            <h1>
-                {months[date.getMonth()]} {date.getFullYear()}
-            </h1>
+            <h1>{format(selectedDate, 'MMM yyyy')}</h1>
             <div className="date">
-                <button onClick={() => selectMonth(-1)}>{'<'}</button>
-                <button onClick={() => setDate(new Date())}>Today</button>
-                <button onClick={() => selectMonth(1)}>{'>'}</button>
+                <button onClick={() => setSelectedDate(subMonths(selectedDate, 1))}>{'<'}</button>
+                <button onClick={() => setSelectedDate(new Date())}>Today</button>
+                <button onClick={() => setSelectedDate(addMonths(selectedDate, 1))}>{'>'}</button>
             </div>
         </div>
         <div className="month">
             {weekDays.map(week => (
                 <div key={week}>{week}</div>
             ))}
-            {[...Array(firstWeekDay)].map((x, i) => (
-                <div key={i}></div>
-            ))}
-            <CalendarDays
-                daysInMonth={daysInMonth}
-                date={date}
-                plants={plants}
-                plantsToday={plantsToday}
-                selectDay={selectDay}
-            />
+            <CalendarDays selectedDate={selectedDate} setSelectedDate={setSelectedDate} plants={plants} />
         </div>
     </div>
 );
 
+const CalendarAside = ({ plants, setSelectedDate, selectedDate }) => (
+    <div className="aside">
+        <div className="calendar-header">
+            <div className="date">
+                <button onClick={() => setSelectedDate(subDays(selectedDate, 1))}>{'<'}</button>
+                {format(selectedDate, 'E d.M')}
+                <button onClick={() => setSelectedDate(addDays(selectedDate, 1))}>{'>'}</button>
+            </div>
+        </div>
+        {plants.length ? <PlantGrid plants={plants} /> : <div>No plants to water today!</div>}
+    </div>
+);
+
 const CalendarView = ({ plants }) => {
-    const [date, setDate] = useState(new Date());
-    const [firstWeekDay, setFirstWeekDay] = useState(getFirstWeekDay(date));
-    const [daysInMonth, setDaysInMonth] = useState(getDaysInMonth(date));
+    const [selectedDate, setSelectedDate] = useState(new Date());
     const [visiblePlants, setVisiblePlants] = useState([]);
 
     useEffect(() => {
-        setFirstWeekDay(getFirstWeekDay(date));
-        setDaysInMonth(getDaysInMonth(date));
-        setVisiblePlants(plantsToday(plants, date));
-    }, [plants, date]);
-
-    const selectMonth = diff => {
-        setDate(new Date(date.getFullYear(), date.getMonth() + diff));
-    };
-
-    const selectDay = day => {
-        setDate(new Date(date.getFullYear(), date.getMonth(), day));
-    };
+        setVisiblePlants(plantsToday(plants, selectedDate));
+    }, [plants, selectedDate]);
 
     return (
         <div id="calendar">
-            <Calendar
-                plants={plants}
-                date={date}
-                setDate={setDate}
-                selectMonth={selectMonth}
-                selectDay={selectDay}
-                firstWeekDay={firstWeekDay}
-                daysInMonth={daysInMonth}
-            />
-            <div className="aside">
-                <div className="calendar-header">
-                    <div className="date">
-                        <button onClick={() => selectDay(date.getDate() - 1)}>{'<'}</button>
-                        {weekDays[(date.getDay() + 6) % 7]} {date.getDate()}.{date.getMonth() + 1}
-                        <button onClick={() => selectDay(date.getDate() + 1)}>{'>'}</button>
-                    </div>
-                </div>
-                {visiblePlants.length ? <PlantGrid plants={visiblePlants} /> : <div>No plants to water today!</div>}
-            </div>
+            <Calendar plants={plants} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
+            <CalendarAside plants={visiblePlants} selectedDate={selectedDate} setSelectedDate={setSelectedDate} />
         </div>
     );
 };
--- client/src/components/Calendar/utils.js
+++ client/src/components/Calendar/utils.js
@@ -1,5 +1,14 @@
 export const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 export const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
-export const getFirstWeekDay = date => (new Date(date.getFullYear(), date.getMonth(), 1).getDay() + 6) % 7;
+export const getFirstWeekday = date => (new Date(date.getFullYear(), date.getMonth(), 1).getDay() + 6) % 7;
+export const getLastWeekday = date => (new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay() + 6) % 7;
 export const getDaysInMonth = date => new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+
+export const plantsToday = (plants, selectedDate) =>
+    plants.filter(plant => new Date(plant.wateringDay).toDateString() === selectedDate.toDateString());
+
+export const getIntervals = (plants, date) =>
+    plantsToday(plants, date)
+        .map(plants => plants.interval) // eslint-disable-next-line
+        .reduce((res, interval) => ((res[interval] = (res[interval] || 0) + 1), res), {});
