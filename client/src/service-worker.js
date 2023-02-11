83ce58e1b54a3e9b49cc611e5f24df9b8e45be62
Maria Tudose <maria.tudose@visma.com>
2023-02-11 17:57:15 +0200

    Set better caching strategy for plant data

--- client/src/service-worker.js
+++ client/src/service-worker.js
@@ -11,7 +11,7 @@ import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
 import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
-import { StaleWhileRevalidate } from 'workbox-strategies';
+import { NetworkFirst, StaleWhileRevalidate } from 'workbox-strategies';
 import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 clientsClaim();
@@ -27,70 +27,70 @@ precacheAndRoute(self.__WB_MANIFEST);
 // https://developers.google.com/web/fundamentals/architecture/app-shell
 const fileExtensionRegexp = new RegExp('/[^/?]+\\.[^/]+$');
 registerRoute(
-  // Return false to exempt requests from being fulfilled by index.html.
-  ({ request, url }) => {
-    // If this isn't a navigation, skip.
-    if (request.mode !== 'navigate') {
-      return false;
-    } // If this is a URL that starts with /_, skip.
+    // Return false to exempt requests from being fulfilled by index.html.
+    ({ request, url }) => {
+        // If this isn't a navigation, skip.
+        if (request.mode !== 'navigate') {
+            return false;
+        } // If this is a URL that starts with /_, skip.
 
-    if (url.pathname.startsWith('/_')) {
-      return false;
-    } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+        if (url.pathname.startsWith('/_')) {
+            return false;
+        } // If this looks like a URL for a resource, because it contains // a file extension, skip.
 
-    if (url.pathname.match(fileExtensionRegexp)) {
-      return false;
-    } // Return true to signal that we want to use the handler.
+        if (url.pathname.match(fileExtensionRegexp)) {
+            return false;
+        } // Return true to signal that we want to use the handler.
 
-    return true;
-  },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
+        return true;
+    },
+    createHandlerBoundToURL(process.env.PUBLIC_URL + '/index.html')
 );
 
 registerRoute(
-  ({ request, url }) => request.destination === 'image' || url.pathname.endsWith('.jpg'),
-  new StaleWhileRevalidate({
-    cacheName: 'images',
-    plugins: [
-      new ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-      }),
-    ],
-  })
+    ({ request, url }) => request.destination === 'image' || url.pathname.endsWith('.jpg'),
+    new StaleWhileRevalidate({
+        cacheName: 'images',
+        plugins: [
+            new ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+            }),
+        ],
+    })
 );
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', event => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
 
 // Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
 registerRoute(
-  ({ request }) =>
-    request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
-  new StaleWhileRevalidate({
-    cacheName: 'assets',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  })
+    ({ request }) =>
+        request.destination === 'style' || request.destination === 'script' || request.destination === 'worker',
+    new StaleWhileRevalidate({
+        cacheName: 'assets',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    })
 );
 
 // Cache plant data
 registerRoute(
-  ({ url }) => url.pathname.startsWith('/api/plants'),
-  new StaleWhileRevalidate({
-    cacheName: 'plants',
-    plugins: [
-      new CacheableResponsePlugin({
-        statuses: [200],
-      }),
-    ],
-  })
+    ({ url }) => url.pathname.startsWith('/api/plants'),
+    new NetworkFirst({
+        cacheName: 'plants',
+        plugins: [
+            new CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    })
 );
