3a70f8e8b4e7e7b2c3c0616aecf1e96833065c26
Maria Tudose <dev.tudose@gmail.com>
2023-04-11 20:24:24 +0300

    Redesign plant modal and implement watering functionality

--- client/src/components/PlantGrid/PlantModal/index.tsx
+++ client/src/components/PlantGrid/PlantModal/index.tsx
@@ -1,7 +1,10 @@
 import { FormEvent, useContext, useEffect, useState } from 'react';
-import { format } from 'date-fns';
+import { addDays, format } from 'date-fns';
 
 import { updatePlant } from '../../../services/plants';
+import { createActions } from '../../../services/actions';
+import { ReactComponent as Drop } from '../../../static/drop.svg';
+import { ReactComponent as Done } from '../../../static/done.svg';
 import { PlantContextProps, PlantContext } from '../../App';
 import PlantPic from '../PlantPic';
 import { getPrevIntervals, weightedAvg } from '../utils';
@@ -15,10 +18,12 @@ interface PlantModalProps {
 }
 
 const PlantModal = ({ plant, visibility, closeModal }: PlantModalProps) => {
-    const [nextWateringDate, setNextWateringDate] = useState<Date>(new Date());
+    const [nextWateringDate, setNextWateringDate] = useState(new Date());
     const [interval, setInterval] = useState('0');
     const [location, setLocation] = useState('');
     const [intervals, setIntervals] = useState<Array<number>>([]);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [waterOnDate, setWaterOnDate] = useState(new Date());
     const { plants, setPlants } = useContext(PlantContext) as PlantContextProps;
 
     useEffect(() => {
@@ -32,6 +37,32 @@ const PlantModal = ({ plant, visibility, closeModal }: PlantModalProps) => {
             setLocation(plant.location);
         }
     }, [plant]);
+
+    const onUpdateWateringDate = (days: number) => setNextWateringDate(addDays(nextWateringDate, days));
+
+    const updateDate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const newDate = new Date(e.target.value);
+        if (!newDate.getTime()) return null;
+        if (e.target.name === 'next-water-date') {
+            setNextWateringDate(newDate);
+        } else {
+            setWaterOnDate(newDate);
+        }
+    };
+
+    const onWater = (e: FormEvent) => {
+        e.preventDefault();
+        if (plant) {
+            const plantBody = { nextWateringDate: addDays(waterOnDate, Number(interval)) };
+            const actionBody = { plantId: plant.id, action: 'water', date: new Date() };
+            updatePlant(plant.id, plantBody).then(updatedPlant => {
+                setPlants(plants.map(plant => (plant.id === updatedPlant.id ? updatedPlant : plant)));
+                setNextWateringDate(plantBody.nextWateringDate);
+                setDropdownOpen(false);
+                createActions([actionBody]);
+            });
+        }
+    };
 
     const onSubmit = (e: FormEvent) => {
         e.preventDefault();
@@ -52,53 +83,87 @@ const PlantModal = ({ plant, visibility, closeModal }: PlantModalProps) => {
                 <div className="plant-pic-container">
                     <PlantPic plant={plant} />
                     <div className="plant-pic-overlay">
-                        <h1>{plant?.name}</h1>
-                        <i>{plant?.userEnteredPlantType}</i>
+                        <div>
+                            <h1>{plant?.name}</h1>
+                            <i>{plant?.userEnteredPlantType}</i>
+                        </div>
+                        <div className="water-dropdown">
+                            <button
+                                className="icon-button"
+                                type="button"
+                                onClick={() => setDropdownOpen(!dropdownOpen)}
+                            >
+                                <Drop />
+                            </button>
+                            <div className={`water-dropdown-menu ${dropdownOpen ? 'open' : ''}`}>
+                                <input
+                                    name="water-date"
+                                    type="date"
+                                    value={format(waterOnDate, 'yyyy-MM-dd')}
+                                    onChange={updateDate}
+                                />
+                                <button className="menu-confirm-button" onClick={onWater}>
+                                    <Done />
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div className="plant-info">
                     <label>
-                        <span>Next Watering date: </span>
-                        <input
-                            type="date"
-                            name="watering"
-                            value={format(nextWateringDate, 'yyyy-MM-dd')}
-                            onChange={e => setNextWateringDate(new Date(e.target.value))}
-                        />
+                        <span className="info-title">Next watering</span>
+                        <div className="info-value">
+                            <button className="add-button" type="button" onClick={() => onUpdateWateringDate(1)}>
+                                +1
+                            </button>
+                            <button className="add-button" type="button" onClick={() => onUpdateWateringDate(2)}>
+                                +2
+                            </button>
+                            <input
+                                name="next-water-date"
+                                type="date"
+                                value={format(nextWateringDate, 'yyyy-MM-dd')}
+                                onChange={updateDate}
+                            />
+                        </div>
                     </label>
                     <label>
-                        <span>Interval: </span>
+                        <span className="info-title">Location</span>
                         <input
-                            className="interval-input"
-                            type="number"
-                            name="interval"
-                            value={interval}
-                            onChange={e => setInterval(e.target.value)}
-                        />
-                        <span> days</span>
-                    </label>
-                    {intervals && (
-                        <>
-                            <p>Calculated interval: {weightedAvg(intervals).toFixed(1)} days</p>
-                            <p>Prev intervals: {intervals.slice(0, 10).join(', ')}</p>
-                        </>
-                    )}
-                    <label>
-                        <span>Location: </span>
-                        <input
-                            className="location-input"
+                            className="info-value location-input"
                             type="string"
                             name="location"
                             value={location}
                             onChange={e => setLocation(e.target.value)}
                         ></input>
                     </label>
+                    <label>
+                        <span className="info-title">Interval (days)</span>
+                        <input
+                            className="info-value interval-input"
+                            type="number"
+                            name="interval"
+                            value={interval}
+                            onChange={e => setInterval(e.target.value)}
+                        />
+                    </label>
+                    {intervals && (
+                        <>
+                            <div>
+                                <span className="info-title">Average interval (days)</span>
+                                <span className="info-value"> {weightedAvg(intervals).toFixed(1)}</span>
+                            </div>
+                            <div>
+                                <span className="info-title">Prev. intervals</span>
+                                <span className="info-value">{intervals.slice(0, 10).join(', ')}</span>
+                            </div>
+                        </>
+                    )}
                 </div>
                 <div className="plant-actions">
-                    <button>Water</button>
-                    <button>+1 days</button>
-                    <button>+2 days</button>
-                    <button type="submit">Confirm</button>
+                    <button className="confirm-button" type="submit">
+                        Confirm
+                    </button>
                 </div>
             </form>
         </div>
